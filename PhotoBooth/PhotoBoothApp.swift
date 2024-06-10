3957e4efbd384745c1207a0c3f82c71dad7e8e75
JeremRav <jeremyrava@outlook.fr>
2024-06-10 13:42:11 +0200

    Initial Commit

---
+++ PhotoBooth/PhotoBoothApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoBoothApp.swift
+//  PhotoBooth
+//
+//  Created by Jérémy Rava on 10/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct PhotoBoothApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
